ab568fedfbbf566e9c0e5cbba6b5a7fa6b26e3cd
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-03-23 14:11:58 +0100

    23/03 Update

--- Modules/M-01-Programming Foundations/D-07-Python for AI(Pandas)/Test.py
+++ Modules/M-01-Programming Foundations/D-07-Python for AI(Pandas)/Test.py
@@ -1,3 +1,21 @@
 import pandas as pd
+import numpy as np
 
-df = pd.dataframe({"Data1": [10,20,30], "Data2":[40,50,60]})
+data = {
+    1: [np.nan, 2, np.nan, 4, 5],
+    2: [np.nan, 2, 3, np.nan, 5],
+    3: [np.nan, 2, np.nan, 4, 5],
+    4: [1, np.nan, 3, np.nan, np.nan],
+    5: [1, 2, np.nan, 4, 5],
+    6: [1, np.nan, 3, np.nan, np.nan],
+    7: [1, 2, 3, 4, 5],
+    8: [1, np.nan, 3, np.nan, np.nan],
+    9: [1, 2, 3, 4, np.nan],
+    10: [1, np.nan, 3, 4, 5]}
+
+df2 = pd.DataFrame({'grps': list('aaabbcaabcccbbc'),
+                   'vals': [12, 345, 3, 1, 45, 14, 4, 52, 54, 23, 235, 21, 57, 3, 87]})
+
+df = pd.DataFrame(data)
+print(df)
+#print(df2)
