19fe11e8d3c71195dcd008d6397857fb7ca6cd94
Udawala Hewage Dilan <79456800+UdawalaHewageDilan@users.noreply.github.com>
2021-06-10 17:47:56 +0200

    MNIST

---
+++ Modules/M-06-Computer Vision/D-04-MNIST/data_handler.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils import data
+from torchvision import datasets, transforms
+
+train = datasets.MNIST('', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
+
+test = datasets.MNIST('', train=False, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ]))
+
+trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
+
